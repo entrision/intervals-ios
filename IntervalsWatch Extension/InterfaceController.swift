0b4ef16c449306950a17efa8a98f5e68d41fb357
Hunter Whittle <hgwhittle@icloud.com>
2015-06-16 10:17:58 -0500

    more work with new watch kit

--- IntervalsWatch Extension/InterfaceController.swift
+++ IntervalsWatch Extension/InterfaceController.swift
@@ -20,6 +20,14 @@ class InterfaceController: WKInterfaceController {
     
     let wcSession = WCSession.defaultSession()
     
+    var messageDict = NSDictionary()
+    var intervalArray = NSArray()
+    var backgroundTimer = NSTimer()
+    var currentIntervalIndex: Int = 0
+    
+    var finished = false
+    var ticking = false
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
@@ -38,6 +46,106 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    //MARK: Actions
+    
+    @IBAction func startButtonTapped() {
+        
+        // Reset
+        if finished {
+            sequenceLoaded()
+            finished = false
+            return
+        }
+        
+        let intervalDict = intervalArray[currentIntervalIndex] as! NSDictionary
+        
+        let duration = NSTimeInterval(intervalDict["duration"] as! NSNumber)
+        let date = NSDate(timeIntervalSinceNow: duration)
+        timer.setDate(date)
+        
+        // Stop
+        if ticking {
+            
+            timer.stop()
+            backgroundTimer.invalidate()
+            sequenceLoaded()
+            
+        }
+        else { // Start
+            
+            timer.start()
+            backgroundTimer = NSTimer.scheduledTimerWithTimeInterval(duration, target: self, selector: Selector("nextInterval"), userInfo: nil, repeats: false)
+            setStartButtonTitle("Stop", color: UIColor.redColor())
+        }
+        
+        self.ticking = !self.ticking
+    }
+    
+    //MARK: Private methods
+    
+    func sequenceLoaded() {
+        
+        timer.setHidden(false)
+        intervalNameLabel.setHidden(false)
+        progressLabel.setHidden(false)
+        actionButton.setHidden(false)
+        setStartButtonTitle("Start", color: UIColor.greenColor())
+        currentIntervalIndex = 0
+        finished = false
+        
+        let sequenceDict = messageDict["sequence"] as! NSDictionary
+        sequenceNameLabel.setText(sequenceDict["name"] as? String)
+        
+        intervalArray = messageDict["intervals"] as! NSArray
+        
+        let firstInterval = intervalArray[currentIntervalIndex] as! NSDictionary
+        intervalNameLabel.setText(firstInterval["title"] as? String)
+        progressLabel.setText("\(intervalArray.indexOfObject(firstInterval)+1) of \(intervalArray.count)")
+        
+        let duration = firstInterval["duration"] as! NSNumber
+        let date = NSDate(timeIntervalSinceNow: NSTimeInterval(duration) + 1)
+        timer.setDate(date)
+    }
+    
+    func nextInterval() {
+        
+        if currentIntervalIndex < intervalArray.count-1 {
+            
+            currentIntervalIndex++
+            
+            let nextInterval = intervalArray[currentIntervalIndex] as! NSDictionary
+            
+            intervalNameLabel.setText(nextInterval["title"] as? String)
+            
+            let text = "\(intervalArray.indexOfObject(nextInterval)+1)"
+            self.progressLabel.setText("\(text) of \(intervalArray.count)")
+            
+            let duration = NSTimeInterval(nextInterval["duration"] as! NSNumber)
+            let date = NSDate(timeIntervalSinceNow: duration)
+            self.timer.setDate(date)
+            self.timer.start()
+            
+            backgroundTimer = NSTimer.scheduledTimerWithTimeInterval(duration, target: self, selector: Selector("nextInterval"), userInfo: nil, repeats: false)
+        }
+        else {
+            
+            intervalNameLabel.setText("Completed")
+            timer.stop()
+            setStartButtonTitle("Reset", color: UIColor.orangeColor())
+            currentIntervalIndex = 0
+            ticking = false
+            finished = true
+        }
+    }
+    
+    func setStartButtonTitle(title: String, color: UIColor) {
+        
+        let string: NSMutableAttributedString = NSMutableAttributedString(string: title)
+        let attributes = [NSForegroundColorAttributeName: color]
+        string.setAttributes(attributes, range:NSMakeRange(0, string.length))
+        actionButton.setAttributedTitle(string)
+    }
 }
 
 //MARK: WCSessionDelegate
@@ -45,14 +153,7 @@ extension InterfaceController : WCSessionDelegate {
     
     func session(session: WCSession, didReceiveMessage message: [String : AnyObject]) {
         
-        let sequenceDict = message["sequence"] as! NSDictionary
-        sequenceNameLabel.setText(sequenceDict["name"] as? String)
-        
-        let intervalArray = message["intervals"] as! NSArray
-        let firstInterval = intervalArray[0] as! NSDictionary
-        intervalNameLabel.setText(firstInterval["title"] as? String)
-        progressLabel.setText("\(intervalArray.indexOfObject(firstInterval)+1) of \(intervalArray.count)")
-        
-        //TODO: Finish implementation
+        messageDict = message
+        sequenceLoaded()
     }
 }
